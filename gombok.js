f4dc960d8ec5da87bfbe827732bf5958437d27bb
Péter <petya.antal@gmail.com>
2024-04-25 12:27:19 +0200

    Cyberbutton-t megcsináltam.

---
+++ gombok.js
@@ -0,0 +1,24 @@
+addToCartButton = document.querySelectorAll(".add-to-cart-button");
+
+document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButton) {
+    addToCartButton.addEventListener('click', function() {
+        addToCartButton.classList.add('added');
+        setTimeout(function(){
+            addToCartButton.classList.remove('added');
+        }, 2000);
+    });
+});
+const button = document.querySelector('.add-to-cart-button');
+
+button.addEventListener('click', function() {
+    button.classList.toggle('clicked');
+});
+
+document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButton) {
+    addToCartButton.addEventListener('click', function() {
+        addToCartButton.classList.add('clicked');
+        setTimeout(function(){
+            addToCartButton.classList.remove('clicked');
+        }, 2000); // Adjusted to match the animation duration
+    });
+});
